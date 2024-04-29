09fb23b6093f9ebfde6446a9424fa0e1ef49b560
LAURA_SOY <100204298+LAURASOY1@users.noreply.github.com>
2024-04-29 16:07:40 +0300

    Add files via upload

---
+++ components/Transaction.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Transaction = (props) => {
+  let { date, description, category, amount } = props.transaction;
+
+  const handleDelete = () => {
+    fetch(`http://localhost:8001/transactions/${props.transaction.id}`, {
+      method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(deletedTransaction => {
+      props.deleteTransactionFun(props.transaction);
+    })
+    .catch(error => {
+      console.error('Error deleting transaction:', error);
+    });
+  };
+
+  return (
+    <tr>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+      <td><button onClick={handleDelete}>X</button></td> 
+    </tr>
+  );
+};
+
+export default Transaction;
